ae4596766237f4701e61d93cdf55f7abed7b7ad3
perlancar (@pc-home) <perlancar@gmail.com>
2015-12-20 15:47:21 +0700

    Initial commit

---
+++ lib/Bencher/Scenario/StartupExporters.pm
@@ -0,0 +1,27 @@
+package Bencher::Scenario::StartupExporters;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+our $scenario = {
+    module_startup => 1,
+
+    participants => [
+        {module=>'Exporters'},
+        {module=>'Exporter::Tiny'},
+        {module=>'Exporter::Tidy'},
+        {module=>'Exporter::Rinci'},
+        {module=>'Sub::Exporter'},
+    ],
+};
+
+1;
+# ABSTRACT: Benchmark startup time of some popular Perl Getopt modules
+
+=head1 SYNOPSIS
+
+ % bencher -m StartupGetoptModules [other option]...
