c8450917af79c8139f073283bfa163d129c5aa4c
perlancar (@pc-home) <perlancar@gmail.com>
2016-03-13 12:33:33 +0700

    Add scenario: Exporters::Exporting

---
+++ lib/Bencher/Scenario/Exporters/Exporting.pm
@@ -0,0 +1,34 @@
+package Bencher::Scenario::Exporters::Exporting;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use File::Temp qw(tempdir);
+use File::Slurper qw(write_text);
+
+my $tempdir = tempdir(CLEANUP => 1);
+write_text("$tempdir/ExampleExporter.pm", 'package ExampleExporter; use Exporter qw(import); our @EXPORT = qw(e1 e2 e3); sub e1{} sub e2{} sub e3{} 1;');
+write_text("$tempdir/ExampleExporterLite.pm", 'package ExampleExporterLite; use Exporter::Lite qw(import); our @EXPORT = qw(e1 e2 e3); sub e1{} sub e2{} sub e3{} 1;');
+write_text("$tempdir/ExamplePERLANCARExporterLite.pm", 'package ExamplePERLANCARExporterLite; use PERLANCAR::Exporter::Lite qw(import); our @EXPORT = qw(e1 e2 e3); sub e1{} sub e2{} sub e3{} 1;');
+
+our $scenario = {
+    summary => 'Benchmark overhead of exporting',
+
+    modules => {
+        'PERLANCAR::Exporter::Lite' => {version=>0.02},
+    },
+
+    participants => [
+        {name=>"Exporter", cmdline => [$^X, "-I$tempdir", "-MExampleExporter", "-e1"]},
+        {name=>"Exporter::Lite", cmdline => [$^X, "-I$tempdir", "-MExampleExporterLite", "-e1"]},
+        {name=>"PERLANCAR::Exporter::Lite", cmdline => [$^X, "-I$tempdir", "-MExamplePERLANCARExporterLite", "-e1"]},
+        {name=>"perl -e1 (baseline)", cmdline => [$^X, "-e1"]},
+    ],
+};
+
+1;
+# ABSTRACT:
